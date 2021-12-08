3381e50dd77a4d9f0c3a1d02574bbcecf8040af2
Koen Van Rulo <koen.vr@pm.me>
2021-12-08 06:42:49 +0100

    combine the network functions

--- shared/network/network.go
+++ shared/network/network.go
@@ -72,3 +72,36 @@ func Create() error {
 
 	return nil
 }
+
+func Destroy() error {
+	fmt.Println("### Destroying private network")
+
+	out, err := exec.List([]string{
+		"-c", "goal network stop -r ./net1",
+	})
+	if len(out) > 0 {
+		fmt.Println()
+		fmt.Println(out)
+	}
+	if nil != err {
+		return err
+	}
+
+	out, err = exec.List([]string{
+		"-c", "goal network delete -r ./net1",
+	})
+	if len(out) > 0 {
+		fmt.Println()
+		fmt.Println(out)
+	}
+	if nil != err {
+		return err
+	}
+
+	exec.List([]string{"-c", "rm -f ./*.rej"})
+	exec.List([]string{"-c", "rm -f ./*.txn"})
+	exec.List([]string{"-c", "rm -f ./*.txs"})
+	exec.List([]string{"-c", "rm -f ./*.frag"})
+
+	return nil
+}
